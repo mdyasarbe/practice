01e1a23f59dc1599696d5630e5352d9ed2973768
mdyasarbe <mdyasarbe@gmail.com>
2021-07-05 12:50:32 +0530

    redef

--- google/versionSorting/Solution.java
+++ google/versionSorting/Solution.java
@@ -1,39 +1,6 @@
 
 import java.util.ArrayList;
 import java.util.List; 
-public class Main{
-
-    public static void main(String args[]){
-
-
-        int n=10,i,j;
-        
-        String l[]=new String[n];
-        System.out.println("Input :");
-        for(i=0;i<n;i++){
-            String ver="";
-            for(j=0;j<Math.round(Math.random()*2)+1;j++){
-                String major=String.valueOf(Math.round(Math.random()*3)); 
-                if(j==0){
-                ver=major;
-               }else{
-                   ver=ver+"."+major;
-               }
-
-            }
-            l[i]=ver;
-            System.out.print(ver+" ");
-        }
-        System.out.println("");
-        
-        System.out.println("Output :");
-        String k[]=Solution.solution(l);
-        System.out.println("length :"+k.length);
-        for(String s :k){
-            System.out.println(s);
-        }
-    }
-}
 class Solution {
     public static String[] solution(String[] l) { 
         GenericTreeNode<LiftVer> verRoot= new GenericTreeNode<LiftVer>(new LiftVer(-1,-1,-1,false));
